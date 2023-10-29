6b943cc623b25bafe8ba2289dcfdb0c7865b29f9
JH <jaehoon0822@gmail.com>
2023-10-29 12:33:28 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ export default async function Home() {
   const flags = await flagsmith.getEnvironmentFlags();
   return (
     <main className="main">
-      <div>
+    <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
       <br />
       {flags.isFeatureEnabled("search") && <input placeholder="Search" />}
+      
     </main>
   );
 }
