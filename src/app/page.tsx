28e949242da6c11a8512fa762855a9329f7fd791
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2024-01-18 07:07:21 -0600

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
+        <p>Based Testing Environment</p>
       </div>
       <Link href="/about">About</Link>
       <br />
