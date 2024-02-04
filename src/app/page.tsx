761ad2a924fa089cac071d9a64cb00bc4f804635
disisdsid <siddhanta.dash@gmail.com>
2024-02-04 21:29:15 +0530

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
-
+import flagsmith from "@utils/flagsmith";
+ export const revalidate = 0
 export default function Home() {
+  const flags = flagsmith.getEnvironmentalflags();
   return (
-    <main className="main">
+    <main className="main" >
       <div>
         <h2>Hello World</h2>
       </div>
       <Link href="/about">About</Link>
+      <br/>
+      {flags.isfeatureEnabled("search") && 
+      <input placeholder="search" />}
     </main>
   );
 }
